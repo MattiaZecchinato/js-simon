b21d1bfc82b3eaa7c537fe343c3cf6f33fac2fbf
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-03-06 15:04:08 +0100

    feat: add random numbers

--- js/script.js
+++ js/script.js
@@ -1,18 +1,39 @@
 // countdown 30 sec
-
 const countdownElement = document.getElementById('countdown');
 
 let count = 10;
 
-const countdown = setInterval(function(){
+const countdown = setInterval(function() {
 
     console.log(count);
     countdownElement.innerText = count;
     count--;
 
     if(count === 0) { 
+        console.log(count);
         countdownElement.innerText = count;
         clearInterval(countdown);
     }
-
 }, 1000);
+
+// random numbers
+const displayNum = 5;
+const maxNumRand = 50;
+
+const numbersRandElement = document.getElementById('numbers-list');
+
+randNum(displayNum, maxNumRand);
+
+
+// FUNCTION
+
+// generate random number
+function randNum(iterations, max) {
+
+    for (let i = 0; i < iterations; i++) {
+
+        const randNum = Math.ceil(Math.random() * max);
+
+        numbersRandElement.innerHTML += `<li>${randNum}</li>`;
+    }
+}
